67e86e616eeffa2455ba1fb92c623f20ec8e1815
A-Hesham11 <a.hesham2215@gmail.com>
2024-04-08 13:15:14 +0200

    sndj

--- src/utils/axios-util.ts
+++ src/utils/axios-util.ts
@@ -9,7 +9,8 @@ import i18n from "../i18n";
 import { notify } from "./toast";
 
 const baseURL =
-  import.meta.env.VITE_BASE_URL || "https://alexon.api.altebr.jewelry/";
+  import.meta.env.VITE_BASE_URL || "https://alexon.altebr.jewelry/";
+  // import.meta.env.VITE_BASE_URL || "https://alexon.api.altebr.jewelry/";
   // import.meta.env.VITE_BASE_URL || "https://dinaa.api.altebr.jewelry";
   // import.meta.env.VITE_BASE_URL || "https://fuga.api.altebr.jewelry/";
 
